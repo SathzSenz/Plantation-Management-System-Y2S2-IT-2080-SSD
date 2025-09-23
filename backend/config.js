f56e42406231a1defa6280085631537a7435afc8
sanuthi_abey <sanuthitabey@gmail.com>
2025-09-23 18:18:58 +0530

    config file

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,9 @@
-export const PORT = process.env.PORT || 5555;
+import dotenv from "dotenv";
 
+// Load environment variables from .env file
+dotenv.config();
+
+export const PORT = process.env.PORT || 5555;
 export const mongoDBURL = process.env.MONGODB_URL;
+export const OPENWEATHER_API_KEY = process.env.OPENWEATHER_API_KEY;
+export const CORS_ORIGINS = process.env.CORS_ORIGINS;
